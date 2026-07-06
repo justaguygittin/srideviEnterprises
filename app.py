eeced983c24b099c38dc12e7cfd860893e68067e
G.Srikar <guddantisrikar2005@gmail.com>
2026-07-06 18:40:51 +0530

    updated app.py for iteration 1

--- app.py
+++ app.py
@@ -1,37 +1,19 @@
 from flask import Flask, render_template
-from database.db import get_connection
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("customer/home.html")
 
 @app.route("/products")
 def products():
+    return render_template("customer/products.html")
 
-    conn = get_connection()
-    cursor = conn.cursor(dictionary=True)
+@app.route("/contact")
+def contact():
+    return render_template("customer/contact.html")
 
-    cursor.execute("""
-        SELECT product_id,
-               product_name,
-               brand,
-               category,
-               offer_price
-        FROM Catalog
-        ORDER BY product_name
-    """)
-
-    products = cursor.fetchall()
-
-    cursor.close()
-    conn.close()
-
-    return render_template(
-        "products.html",
-        products=products
-    )
 
 if __name__ == "__main__":
     app.run(debug=True)
